89e03ed766cff0dc2dfc6ccf806433850e2ec213
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-09-16 22:53:20 +0530

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,1 +1,23 @@
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        if(low<high){
+            int p=partition(arr,low,high);
+            quickSort(arr,low,p-1);
+            quickSort(arr,p+1,high);
+        }
+    }
 
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        int piv=arr[low];
+        int i=low,j=high;
+        while(i<j){
+            while(arr[i]<=piv && i<=high-1 ) i++;
+            while(arr[j]>piv && j>=low+1) j--;
+            if(i<j) swap(arr[i],arr[j]);
+        }
+        swap(arr[j],arr[low]);
+        return j;
+    }
+};
