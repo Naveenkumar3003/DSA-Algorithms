9c5c6c18078500f034571697728df1567bd7fde1
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-09-16 23:19:03 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+  public:
+    void mergeSort(vector<int>& arr, int l, int r) {
+        if(l==r) return;
+        int m=(l+r)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+    void merge(vector<int>& arr,int l,int mid, int r){
+        vector<int> temp;
+        int left=l;
+        int right=mid+1;
+        while(left<=mid && right<=r){
+            if(arr[left]<=arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while(right<=r){
+            temp.push_back(arr[right]);
+            right++;
+        }
+        for(int i=l;i<=r;i++){
+            arr[i]=temp[i-l];
+        }   
+    }
+};
