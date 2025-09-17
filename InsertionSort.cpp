d870763ed9dcba272bd2d29e7704632aa163f449
Naveenkumar3003 <naveengoogl2006@gmail.com>
2025-09-17 22:03:40 +0530

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        int n=arr.size();
+        int flag=0;
+        for(int i=n-1;i>=1;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]) {
+                    swap(arr[j],arr[j+1]);
+                    flag=1;
+                }
+            }
+            if(flag==0) break;
+        }
+        
+    }
+};
